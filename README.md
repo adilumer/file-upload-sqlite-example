6ea34fa5a2ffd1b961e574b51675c3b45a0bca72
Adil Umer <haris2431@gmail.com>
2024-10-21 10:21:35 +0300

    notes

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # File Upload & SQLite
 
 This is a sample project for uploading files and storing the info in an SQLite database, developed during a training session.
+
+## Konular
+
+- Binary data yazma sorunu
+- Cache - REDIS
+- SOAP veri Okuma
+- email/sms gönderimi
--- src/lib/http-server.js
+++ src/lib/http-server.js
@@ -1,7 +1,10 @@
 const http = require("http");
+const https = require("https");
 
 function createHttpServer(requestHandler){
   const httpServer = http.createServer(requestHandler);
+  //const httpsOptions = {}
+  //const httpsServer = https.createServer(httpsOptions, requestHandler);
   
   httpServer.listen(httpServerPort, () => {
     console.log(httpServerPort, "portunda dinliyorum");
